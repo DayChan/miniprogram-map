5bbbd9cbe4ce1b8028a40c8fe2d6dba1b98a4bfd
Chenduoru <chenduoru21@qq.com>
2018-04-30 00:55:46 +0800

    v-0.02

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  //调试模式开关，调试模式下只调用本地数据
+  debug: true,
+  //学校数据配置名称，学校英文缩写
+  school: "uestc",
+  //高德路线规划密钥，必须加入https://restapi.amap.com为request合法域名
+  //密钥申请地址http://lbs.amap.com/api/javascript-api/summary/
+  key: "ce222de9a8d12feef03ede6d6cfa0de1",
+  //地图更新地址，用于热修补，无需每次都提交审核
+  updateUrl: "https://www.qq.com/json.json",
+  //图片CDN域名
+  imgCDN: "https://www.daychan.house/"
+}
---
+++ resources/uestc.js
@@ -0,0 +1,579 @@
+//学校简介
+module.exports.introduce = {
+  "shortName": "成电", //学校名字两个字缩写
+  "name": "电子科技大学", //学校名称
+  "images": "校园导览/gdst.jpg", // 首页图片地址
+  "share": "校园导览/share.jpg", //分享时展示图片的地址
+  "latitude": "22.972816",
+  "longitude": "113.756003",
+  "img": ["校园导览/校门/北门.jpg", "校园导览/教学楼/全景.jpg"],  //简介图片
+  "description": "",
+  "mapCopyright": "招生宣传服务中心"
+}
+//学校地图数据
+module.exports.map = [
+  {
+    "name": "餐厅",
+    "scale": 16,
+    "data": [
+      {
+        "name": "桂苑餐厅",
+        "latitude": "30.674232",
+        "longitude": "104.096991",
+        "iconPath": "/img/ico/jd.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/风华餐厅.png"
+        ],
+        "description": "a"
+      },
+      {
+        "name": "阳光餐厅",
+        "latitude": "30.67689",
+        "longitude": "104.100172",
+        "iconPath": "/img/ico/jd.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/阳光.png"
+        ],
+        "description": "a"
+      },
+      {
+        "name": "学生活动中心",
+        "floor": "2楼",
+        "latitude": "30.676899",
+        "longitude": "104.100199",
+        "iconPath": "/img/ico/jd.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/学生活动中心.png",
+          "miniImage/学生活动中心2.png"
+        ],
+        "description": "　a"
+      },
+      {
+        "name": "风华餐厅",
+        "latitude": "30.677074",
+        "longitude": "104.100172",
+        "iconPath": "/img/ico/jd.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/阳光.png"
+        ],
+        "description": "a"
+      },
+      {
+        "name": "万友餐厅",
+        "floor": "2楼",
+        "latitude": "30.67701",
+        "longitude": "104.100746",
+        "iconPath": "/img/ico/jd.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/阳光.png"
+        ],
+        "description": "a"
+      },
+      {
+        "name": "风华清真",
+        "latitude": "30.676987",
+        "longitude": "104.101626",
+        "iconPath": "/img/ico/jd.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/阳光.png"
+        ],
+        "description": "a"
+      }
+    ]
+  },
+  {
+    "name": "教学楼",
+    "scale": 16,
+    "data": [
+      {
+        "name": "第一教学楼",
+        "latitude": "30.674034",
+        "longitude": "104.102779",
+        "iconPath": "/img/ico/jxl.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/一教.png"
+        ],
+        "description": "　1"
+      },
+      {
+        "name": "第二教学楼",
+        "latitude": "30.674527",
+        "longitude": "104.102736",
+        "iconPath": "/img/ico/jxl.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/二教.png"
+        ],
+        "description": "a"
+      },
+      {
+        "name": "第三教学楼",
+        "latitude": "30.675861",
+        "longitude": "104.10309",
+        "iconPath": "/img/ico/jxl.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/三教.png",
+          "miniImage/三教2.png"
+        ],
+        "description": "　3号教学楼"
+      },
+      {
+        "name": "第四教学楼",
+        "latitude": "30.675427",
+        "longitude": "104.09955",
+        "iconPath": "/img/ico/jxl.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/阳光.png"
+        ],
+        "description": "　a"
+      },
+      {
+        "name": "逸夫楼",
+        "latitude": "30.675233",
+        "longitude": "104.103112",
+        "iconPath": "/img/ico/jxl.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/逸夫楼.png"
+        ],
+        "description": "　a"
+      }
+    ]
+  },
+  {
+    "name": "景点",
+    "scale": 16,
+    "data": [
+      {
+        "name": "主楼",
+        "latitude": "30.674108",
+        "longitude": "104.101143",
+        "iconPath": "/img/ico/xm.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/主楼.png",
+          "miniImage/主楼2.png"
+        ],
+        "description": "a"
+      },
+      {
+        "name": "图书馆",
+        "latitude": "30.673725",
+        "longitude": "104.09926",
+        "iconPath": "/img/ico/xm.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "校园导览/校门/东门.jpg"
+        ],
+        "description": "a"
+      },
+      {
+        "name": "学生公寓11号楼",
+        "latitude": "30.678128",
+        "longitude": "104.098546",
+        "iconPath": "/img/ico/girl_ss.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/游泳中心.png"
+        ],
+        "description": "学生公寓11号楼"
+      }
+    ]
+  },
+  {
+    "scale": 16,
+    "name": "学院楼",
+    "data": [
+      {
+        "name": "科技实验大楼",
+        "latitude": "30.675792",
+        "longitude": "104.099405",
+        "iconPath": "/img/ico/xyfu.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "校园导览/创业楼/广科招生办301.jpg"
+        ],
+        "description": ""
+      },
+      {
+        "name": "通信大楼",
+        "latitude": "30.67612",
+        "longitude": "104.100805",
+        "iconPath": "/img/ico/xyfu.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/通信楼1.png",
+          "miniImage/通信楼2.png"
+        ],
+        "description": " "
+      },
+      {
+        "name": "继续教育学院",
+        "latitude": "30.674029",
+        "longitude": "104.096776",
+        "iconPath": "/img/ico/xyfu.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/继续教育学院.png"
+        ],
+        "description": "a"
+      },
+      {
+        "name": "微电子与固体电子楼（电子学院楼）",
+        "latitude": "30.675252",
+        "longitude": "104.101207",
+        "iconPath": "/img/ico/xyfu.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/微固.png"
+        ],
+        "description": " a"
+      },
+      {
+        "name": "光电楼",
+        "latitude": "30.674906",
+        "longitude": "104.100027",
+        "iconPath": "/img/ico/xyfu.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/继续教育学院.png"
+        ],
+        "description": "a"
+      },
+      {
+        "name": "物理学院楼",
+        "latitude": "30.675722",
+        "longitude": "104.102366",
+        "iconPath": "/img/ico/xyfu.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/物理学院.png",
+          "miniImage/物理学院2.png"
+        ],
+        "description": "a"
+      },
+      {
+        "name": "信软楼",
+        "latitude": "30.676396",
+        "longitude": "104.102409",
+        "iconPath": "/img/ico/xyfu.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/信软楼.png",
+          "miniImage/信软楼2.png"
+        ],
+        "description": " a"
+      }
+    ]
+  },
+  {
+    "scale": 16,
+    "name": "体育场",
+    "data": [
+      {
+        "name": "体育馆",
+        "latitude": "30.675076",
+        "longitude": "104.097801",
+        "iconPath": "/img/ico/xyfu.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/信软楼.png"
+        ],
+        "description": "a"
+      },
+      {
+        "name": "足球场",
+        "latitude": "30.675109",
+        "longitude": "104.098402",
+        "iconPath": "/img/ico/xyfu.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/信软楼.png"
+        ],
+        "description": "s"
+      },
+      {
+        "name": "网球场",
+        "latitude": "30.677725",
+        "longitude": "104.098074",
+        "iconPath": "/img/ico/xyfu.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/网球场.png"
+        ],
+        "description": "a"
+      },
+      {
+        "name": "篮球场",
+        "latitude": "30.676331",
+        "longitude": "104.099721",
+        "iconPath": "/img/ico/xyfu.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/篮球场.png"
+        ],
+        "description": " a "
+      },
+      {
+        "name": "游泳池",
+        "latitude": "30.677268",
+        "longitude": "104.09749",
+        "iconPath": "/img/ico/xyfu.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/游泳中心.png",
+          "miniImage/游泳中心2.png",
+          "miniImage/游泳中心3.png"
+        ],
+        "description": "s"
+      }
+    ]
+  },
+  {
+    "scale": 16,
+    "name": "宿舍",
+    "data": [
+      {
+        "name": "欣苑1栋",
+        "latitude": "30.674317",
+        "longitude": "104.096041",
+        "iconPath": "/img/ico/boy_ss.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/欣1.png",
+          "miniImage/欣1（2）.png"
+        ],
+        "description": "欣苑1栋"
+      },
+      {
+        "name": "欣苑2栋",
+        "latitude": "30.674576",
+        "longitude": "104.095988",
+        "iconPath": "/img/ico/boy_ss.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/欣2.png"
+        ],
+        "description": "欣苑2栋"
+      },
+      {
+        "name": "欣苑3栋",
+        "latitude": "30.674756",
+        "longitude": "104.095714",
+        "iconPath": "/img/ico/boy_ss.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/欣3.png",
+          "miniImage/欣3（2）.png"
+        ],
+        "description": "欣苑3栋"
+      },
+      {
+        "name": "欣苑4栋",
+        "latitude": "30.675259",
+        "longitude": "104.095719",
+        "iconPath": "/img/ico/boy_ss.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/欣4.png",
+          "miniImage/欣4（2）.png"
+        ],
+        "description": "欣苑4栋"
+      },
+      {
+        "name": "欣苑5栋",
+        "latitude": "30.674682",
+        "longitude": "104.096524",
+        "iconPath": "/img/ico/boy_ss.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/欣5.png",
+          "miniImage/欣5（2）.png"
+        ],
+        "description": "欣苑5栋"
+      },
+      {
+        "name": "欣苑6栋",
+        "latitude": "30.673736",
+        "longitude": "104.09625",
+        "iconPath": "/img/ico/girl_ss.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/游泳中心.png"
+        ],
+        "description": "欣苑6栋"
+      },
+      {
+        "name": "学生公寓11号楼",
+        "latitude": "30.678128",
+        "longitude": "104.098546",
+        "iconPath": "/img/ico/girl_ss.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/游泳中心.png"
+        ],
+        "description": "学生公寓11号楼"
+      },
+      {
+        "name": "学生公寓12号楼",
+        "latitude": "30.678396",
+        "longitude": "104.098021",
+        "iconPath": "/img/ico/boy_ss.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/12栋.png",
+          "miniImage/12栋2.png"
+        ],
+        "description": "学生公寓12号楼"
+      },
+      {
+        "name": "学生公寓13号楼",
+        "latitude": "30.677778",
+        "longitude": "104.100134",
+        "iconPath": "/img/ico/boy_ss.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/13栋.png"
+        ],
+        "description": "学生公寓13号楼"
+      },
+      {
+        "name": "学生公寓14号楼",
+        "latitude": "30.678041",
+        "longitude": "104.099533",
+        "iconPath": "/img/ico/girl_ss.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/14栋.png"
+        ],
+        "description": "学生公寓14号楼"
+      },
+      {
+        "name": "学生公寓15号楼",
+        "latitude": "30.678571",
+        "longitude": "104.098622",
+        "iconPath": "/img/ico/girl_ss.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/15栋.png",
+          "miniImage/15栋2.png"
+        ],
+        "description": "学生公寓15号楼"
+      },
+      {
+        "name": "学生公寓16号楼",
+        "latitude": "30.678405",
+        "longitude": "104.097398",
+        "iconPath": "/img/ico/girl_ss.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/16栋.png",
+          "miniImage/16栋2.png"
+        ],
+        "description": "学生公寓16号楼"
+      },
+      {
+        "name": "学生公寓17号楼",
+        "latitude": "30.678581",
+        "longitude": "104.097854",
+        "iconPath": "/img/ico/girl_ss.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/17栋.png"
+        ],
+        "description": "学生公寓17号楼"
+      },
+      {
+        "name": "学生公寓18号楼",
+        "latitude": "30.678756",
+        "longitude": "104.098412",
+        "iconPath": "/img/ico/boy_ss.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "miniImage/18栋.png",
+          "miniImage/18栋2.png"
+        ],
+        "description": "学生公寓18号楼"
+      }
+    ]
+  },
+  {
+    "name": "其他",
+    "scale": 16,
+    "data": [
+      {
+        "name": "校医院",
+        "latitude": "30.674221",
+        "longitude": "104.103793",
+        "iconPath": "/img/ico/xm.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "校园导览/校门/北门.jpg"
+        ],
+        "description": "a"
+      },
+      {
+        "name": "电子科大宾馆",
+        "latitude": "30.673067",
+        "longitude": "104.099453",
+        "iconPath": "/img/ico/xm.png",
+        "width": "30",
+        "height": "30",
+        "img": [
+          "校园导览/校门/东门.jpg"
+        ],
+        "description": "a"
+      }
+    ]
+  }
+]
